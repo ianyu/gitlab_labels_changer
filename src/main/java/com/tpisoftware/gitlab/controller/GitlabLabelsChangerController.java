f13b7df21c9db63273af8a6151f9b019d96e553b
tpi <tpi@192.168.100.76>
2022-12-09 14:51:34 +0800

    change RequestMethod PUT for GitLab API invoke

--- src/main/java/com/tpisoftware/gitlab/controller/GitlabLabelsChangerController.java
+++ src/main/java/com/tpisoftware/gitlab/controller/GitlabLabelsChangerController.java
@@ -29,7 +29,7 @@ public class GitlabLabelsChangerController {
 	@Autowired
 	GitLabMergeRequestsEventsService gitLabMergeRequestsEventsService;
 
-	@RequestMapping(value = "/labelsActionPush", method = RequestMethod.POST)
+	@RequestMapping(value = "/labelsActionPush", method = RequestMethod.PUT)
 	public Object labelsActionPush(@RequestBody String payload) {
 
 		StringBuffer localApiUrl = 
@@ -43,7 +43,7 @@ public class GitlabLabelsChangerController {
 		
 	}
 	
-	@RequestMapping(value = "/labelsActionMR", method = RequestMethod.POST)
+	@RequestMapping(value = "/labelsActionMR", method = RequestMethod.PUT)
 	public Object labelsActionMR(@RequestBody String payload) {
 
 		StringBuffer localApiUrl = 
